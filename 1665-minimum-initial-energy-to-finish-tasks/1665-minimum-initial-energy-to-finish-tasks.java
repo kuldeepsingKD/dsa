7447511d8e19cbeda1945caa6de009353639473f
kuldeepsingKD <168539448+kuldeepsingKD@users.noreply.github.com>
2026-07-25 20:20:45 +0530

    Time: 24 ms (50.7%), Space: 114 MB (48.37%) - LeetHub

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.java
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.java
@@ -1,42 +1,22 @@
 class Solution {
-    private boolean isPossible(int mid, int[][] tasks) {
-        for(int[] task : tasks) {
+    public int minimumEffort(int[][] tasks) {
+            Arrays.sort(tasks, (a, b) -> (b[1] - b[0]) - (a[1] - a[0]));
+
+        int energy = 0;
+        int current = 0;
+
+        for (int[] task : tasks) {
             int actual = task[0];
             int minimum = task[1];
 
-            if(minimum > mid){
-                return false;
+            if (current < minimum) {
+                energy += minimum - current;
+                current = minimum;
             }
 
-            mid -= actual;
+            current -= actual;
         }
 
-        return true;
-    }
-    public int minimumEffort(int[][] tasks) {
-        int n = tasks.length;
-        int result = Integer.MAX_VALUE;
-
-        int l = 0;
-        int r = (int)1e9;
-
-          Arrays.sort(tasks, (task1, task2) -> {
-            int diff1 = task1[1] - task1[0];
-            int diff2 = task2[1] - task2[0];
-
-            return diff2 - diff1;
-        });
-
-        while(l <= r) {
-            int mid = l+(r-l)/2;
-            if(isPossible(mid, tasks)){
-                result = mid;
-                r = mid - 1;
-            }else{
-                l = mid+1;
-            }
-        }
-
-        return result;
+        return energy;
     }
 }
