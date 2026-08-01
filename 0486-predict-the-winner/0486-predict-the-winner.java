ade6f56b5fb61094c3ff3e264125b45bf9f7be65
kuldeepsingKD <168539448+kuldeepsingKD@users.noreply.github.com>
2026-08-01 19:30:38 +0530

    Time: 0 ms (100%), Space: 42.8 MB (57.54%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,14 +1,20 @@
 class Solution {
+    int[][] t = new int[23][23];
     private int solve(int i, int j, int[] nums) {
         if(i > j) return 0;
         if(i == j) return nums[i];
 
+          if (t[i][j] != -1)
+            return t[i][j];
+
         int takes_i = nums[i] + Math.min(solve(i+2, j, nums), solve(i+1, j-1, nums));
         int takes_j = nums[j] + Math.min(solve(i+1, j-1, nums), solve(i, j-2, nums));
 
-        return Math.max(takes_i, takes_j);
+        return t[i][j] = Math.max(takes_i, takes_j);
     }
     public boolean predictTheWinner(int[] nums) {
+         for (int[] row : t)
+            Arrays.fill(row, -1);
         int n = nums.length;
         int totalScore = 0;
         for(int num : nums) {
