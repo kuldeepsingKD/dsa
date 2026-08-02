3a0af6844286bdaea13e422a48c204ac21f57141
kuldeepsingKD <168539448+kuldeepsingKD@users.noreply.github.com>
2026-08-02 18:28:25 +0530

    Time: 10 ms (10.85%), Space: 49.5 MB (7.53%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,5 +1,21 @@
 class Solution {
+     Integer[][] dp;
+     private int solve(int i, int j, int[] piles) {
+        if(i == j) return piles[i];
+
+        if(dp[i][j] != null) {
+            return dp[i][j];
+        }
+
+        int left = piles[i] - solve(i+1, j, piles);
+        int right = piles[j] - solve(i, j-1, piles);
+
+        return dp[i][j] = Math.max(left, right);
+     }
     public boolean stoneGame(int[] piles) {
-        return true;
+        int n = piles.length;
+        dp = new Integer[n][n];
+
+        return solve(0, n-1, piles) > 0;
     }
 }
