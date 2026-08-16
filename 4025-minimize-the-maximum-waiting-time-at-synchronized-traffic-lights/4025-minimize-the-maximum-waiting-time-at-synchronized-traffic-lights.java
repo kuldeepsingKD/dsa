08e9758f3739b7fb22f99fd756824e80b4c472a0
kuldeepsingKD <168539448+kuldeepsingKD@users.noreply.github.com>
2026-08-16 12:05:40 +0530

    Time: 2 ms (100%), Space: 121.7 MB (47.62%) - LeetHub

--- 4025-minimize-the-maximum-waiting-time-at-synchronized-traffic-lights/4025-minimize-the-maximum-waiting-time-at-synchronized-traffic-lights.java
+++ 4025-minimize-the-maximum-waiting-time-at-synchronized-traffic-lights/4025-minimize-the-maximum-waiting-time-at-synchronized-traffic-lights.java
@@ -1,41 +1,18 @@
 class Solution {
-    private boolean isValid(int W, int[] arrivalTime, int maxGreen, int period) {
-        for(int t : arrivalTime) {
-            int r = t % period;
-
-
-            if(r < maxGreen){
-                continue;
-            }
-
-            if(period - r > W) {
-                return false;
-            }
-        }
-        return true;
-    }
     public int minPenalty(int period, int[] lights, int[] arrivalTime) {
-        int maxGreen = 0;
-        for(int light : lights) {
-            maxGreen = Math.max(maxGreen, light);
-
+          int mxPenalty = 0;
+        int mx = 0;
+        for(int i : lights){
+            mx = Math.max(mx, i);
         }
-
-        int low = 0;
-        int high = period;
-        int ans = 0;
-
-        while(low <= high) {
-            int mid = low + (high - low)/2;
-
-            if(isValid(mid, arrivalTime, maxGreen, period)) {
-                ans = mid;
-                high = mid-1;
-            }else {
-                low = mid+1;
+        for(int i : arrivalTime){
+            if(i >= mx){
+                int a = i%period;
+                if(a >= mx){
+                    mxPenalty = Math.max(mxPenalty, period-a);
+                }
             }
         }
-
-        return ans;
+        return mxPenalty;
     }
 }
