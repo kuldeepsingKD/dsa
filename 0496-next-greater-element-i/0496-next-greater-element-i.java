80d972b04560ae05b1b785081d5d9696c56e837d
kuldeepsingKD <168539448+kuldeepsingKD@users.noreply.github.com>
2026-09-09 08:44:10 +0530

    Time: 3 ms (93.05%), Space: 45.4 MB (35.77%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> st = new Stack<>();
+
+        for(int num : nums2) {
+            while(!st.isEmpty() && st.peek() < num) {
+                map.put(st.pop(), num);
+            }
+            st.push(num);
+        }
+
+        while(!st.isEmpty()){
+            map.put(st.pop(), -1);
+        }
+
+        int[] result = new int[nums1.length];
+        for(int i = 0; i < nums1.length; i++) {
+            result[i] = map.get(nums1[i]);
+        }
+
+        return result;
+    }
+}
