bc420b0bfada5094f9c33486a3166a4fcfc81ae1
kuldeepsingKD <168539448+kuldeepsingKD@users.noreply.github.com>
2026-09-18 19:43:42 +0530

    Time: 1 ms (100%), Space: 47.2 MB (83.74%) - LeetHub

--- 4052-cyclically-shift-rows-and-columns/4052-cyclically-shift-rows-and-columns.java
+++ 4052-cyclically-shift-rows-and-columns/4052-cyclically-shift-rows-and-columns.java
@@ -2,26 +2,17 @@ class Solution {
     public int[][] cyclicShift(int n, int[][] grid, int[] rowShift, int[] colShift) {
          
         
-        // Step 1: Row shifts apply karne ke liye temporary matrix
-        int[][] afterRowShift = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                // Left shift ke baad naya column index calculate karein
-                int newJ = (j - rowShift[i] % n + n) % n;
-                afterRowShift[i][newJ] = grid[i][j];
+      int[][] ans = new int[n][n];
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                int nc = (j - rowShift[i] + n) % n;
+                int nr = (i - colShift[nc] + n) % n;
+
+                ans[nr][nc] = grid[i][j];
             }
         }
-        
-        // Step 2: Column shifts apply karne ke liye final matrix
-        int[][] finalGrid = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                // Upward shift ke baad naya row index calculate karein
-                int newI = (i - colShift[j] % n + n) % n;
-                finalGrid[newI][j] = afterRowShift[i][j];
-            }
-        }
-        
-        return finalGrid;
+
+        return ans;
     }
 }
